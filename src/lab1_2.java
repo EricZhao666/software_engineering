b7fc461aab52c7fdf16335a7bf5436da50c7b445
EricZhao666 <1339278927@qq.com>
2022-10-22 20:25:54 +0800

    将部分代码封装成函数

--- src/lab1_2.java
+++ src/lab1_2.java
@@ -7,8 +7,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class lab1_2 {
+    static int total_num=0,switch_num=0,if_else_num=0,if_else_if_else_num=0;
 	public static void main(String [] args) throws Exception {
-		int total_num=0,switch_num=0,if_else_num=0,if_else_if_else_num=0;
+
 		String code="";
         String fileName ="src/samplec.c";
         FileReader fileReader = new FileReader(fileName);
@@ -27,15 +28,53 @@ public class lab1_2 {
                 + "transient、try、void、volatile、while";//all keywords
         String []keyArr=keywords.split("、");
 
-        //check total key word
+
+
+
+
+
+        //Compute if-else and if-elseif-else
+        Pattern p = Pattern.compile("else\\s*if|else|if");
+        Matcher matcher=p.matcher(code);
+        Stack<String> s = new Stack();
+        int count=0;
+        while(matcher.find()) {
+            String temp=code.substring(matcher.start(),matcher.end());
+            s.push(temp);
+        }
+        while(!s.isEmpty()) {
+            String temp=s.pop();
+            if(temp.equals("else")) {
+                String temp2=s.pop();
+                if(temp2.equals("if")) if_else_num++;
+                else if(temp2.equals("else if")) if_else_if_else_num++;
+                else if(temp2.equals("else")) count+=2;
+            }else if(count>0) {
+                if(temp.equals("else if")) if_else_if_else_num++;
+                else if(temp.equals("if")) if_else_num++;
+                count--;
+            }
+        }
+
+
+
+        System.out.println("\nif else num is "+if_else_num);
+        System.out.println("if else if else num is "+if_else_if_else_num);
+	}
+
+    //check total key word
+    public static void Find_All(String[] keyArr,String code){
         for(int i=0;i<keyArr.length;i++) {
             Pattern p=Pattern.compile("[^a-z]"+keyArr[i]+"[^a-z]");
             Matcher matcher=p.matcher(code);
             while(matcher.find()) {
                 total_num++;
             }
+            System.out.println("total num is "+total_num);
         }
-
+    }
+    //check switch and case
+    public static void Find_Switch(String code){
         //check switch
         Pattern p=Pattern.compile("switch");
         Matcher matcher=p.matcher(code);
@@ -57,52 +96,11 @@ public class lab1_2 {
             }
             case_num.add(temp_case_num);
         }
-
-        //Compute if-else and if-elseif-else
-        p = Pattern.compile("else\\s*if|else|if");
-        matcher=p.matcher(code);
-        Stack<String> s = new Stack();
-        int count=0;
-        while(matcher.find()) {
-            String temp=code.substring(matcher.start(),matcher.end());
-            s.push(temp);
-        }
-        while(!s.isEmpty()) {
-            String temp=s.pop();
-            if(temp.equals("else")) {
-                String temp2=s.pop();
-                if(temp2.equals("if")) if_else_num++;
-                else if(temp2.equals("else if")) if_else_if_else_num++;
-                else if(temp2.equals("else")) count+=2;
-            }else if(count>0) {
-                if(temp.equals("else if")) if_else_if_else_num++;
-                else if(temp.equals("if")) if_else_num++;
-                count--;
-            }
-        }
-
-        System.out.println("total num is "+total_num);
         System.out.println("switch num is "+switch_num);
         System.out.print("case num are ");
         for(int i=0;i<case_num.size();i++) {
             System.out.print(case_num.get(i)+" ");
         }
-        System.out.println("\nif else num is "+if_else_num);
-        System.out.println("if else if else num is "+if_else_if_else_num);
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-	}
+    }
 
 }
